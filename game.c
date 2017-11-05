35fe97c7102d30430e98b32136c3bd084d43d284
Giacom <giacomand@gmail.com>
2017-11-05 13:32:14 +0000

    Trying out loading graphics and zooming.

--- game.c
+++ game.c
@@ -2,18 +2,26 @@
 #include <math.h>
 
 #include "allegro5/allegro_font.h"
+#include "allegro5/allegro_image.h"
 
 #include "game.h"
 
+int scale = 4;
+int screen_width = 1024, screen_height = 512;
+
 double x = 0, y = 0;
-int w = 100, h = 100;
+int w = 20, h = 20;
+long ticks = 0;
 
 ALLEGRO_FONT* default_font = NULL;
 ALLEGRO_BITMAP* square = NULL;
 
+ALLEGRO_BITMAP* test_sprite = NULL;
+
 void game_run() {
     al_init();
     al_init_font_addon();
+    al_init_image_addon();
 
     default_font = al_create_builtin_font();
 
@@ -21,7 +29,20 @@ void game_run() {
     al_set_target_bitmap(square);
     al_clear_to_color(al_map_rgba(255, 255, 255, 255));
 
-    ALLEGRO_DISPLAY* display = al_create_display(512, 512);
+    test_sprite = al_load_bitmap("lemmings.tga");
+    if (!test_sprite) {
+        printf("Error loading sprite.\n");
+        return;
+    }
+
+    ALLEGRO_DISPLAY* display = al_create_display(screen_width, screen_height);
+
+    // Set Zoom for display
+    ALLEGRO_TRANSFORM transform;
+    al_identity_transform(&transform);
+    al_scale_transform(&transform, scale, scale);
+    al_use_transform(&transform);
+
     ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
 
     al_register_event_source(queue, al_get_display_event_source(display));
@@ -30,8 +51,10 @@ void game_run() {
     double previous_time = al_get_time();
     double current_time = previous_time;
 
-    while (running) {
+    printf("%lu", sizeof(ALLEGRO_EVENT));
 
+    while (running) {
+        ticks++;
         previous_time = current_time;
         current_time = al_get_time();
 
@@ -39,7 +62,8 @@ void game_run() {
 
         ALLEGRO_EVENT event;
         while (al_get_next_event(queue, &event)) {
-            if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+            enum InputResult result = game_handle_input(&event);
+            if (result == QUIT_GAME) {
                 running = false;
             }
         }
@@ -47,8 +71,13 @@ void game_run() {
         game_update(delta);
         game_draw(delta);
     }
+}
 
-    printf("Hello world\n");
+enum InputResult game_handle_input(ALLEGRO_EVENT* event) {
+    if (event->type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+        return QUIT_GAME;
+    }
+    return NOTHING;
 }
 
 void game_update(double delta) {
@@ -61,8 +90,9 @@ void game_update(double delta) {
 }
 
 void game_draw(double delta) {
-    al_clear_to_color(al_map_rgba(0, 0, 0, 255));
+    al_clear_to_color(al_map_rgba(0, 255, 255, 255));
     al_draw_bitmap(square, x, y, 0);
-    al_draw_textf(default_font, al_map_rgba(255, 255, 0, 255), 0, 0, ALLEGRO_ALIGN_LEFT, "%.2f", 1.0 / delta);
+    al_draw_bitmap_region(test_sprite, 0, 0, 5, 8, x, y + h * 2, 0);
+    al_draw_textf(default_font, al_map_rgba(255, 255, 0, 255), 0, 0, ALLEGRO_ALIGN_LEFT, "%.0f", 1.0 / delta);
     al_flip_display();
 }
--- game.h
+++ game.h
@@ -1,7 +1,16 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include "allegro5/allegro.h"
+
+enum InputResult {
+    QUIT_GAME,
+    NOTHING
+};
+
+
 void game_run(void);
+enum InputResult game_handle_input(ALLEGRO_EVENT* event);
 void game_update(double delta);
 void game_draw(double delta);
 
